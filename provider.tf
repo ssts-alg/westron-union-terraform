490904d14475b0498c97110a9032e7b25ab77c53
sureshbabu <sureshbabu@gmail.com>
2022-07-21 07:55:33 +0530

    terraform code 3

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "westron-union-terraform-statefile-380377972418-us-east-1"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-wu"
+  }
+}
