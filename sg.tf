3341ef3881f72aa625d73290b0b7a070844e0274
sureshbabu <sureshbabu@gmail.com>
2022-07-22 07:46:02 +0530

    allow ssh

--- sg.tf
+++ sg.tf
@@ -54,6 +54,14 @@ resource "aws_security_group" "allow_ssh1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
