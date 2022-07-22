ff0664362ffad53f5982f39372de3c46a1f5d307
sureshbabu <sureshbabu@gmail.com>
2022-07-22 08:22:57 +0530

    modify files

--- sg.tf
+++ sg.tf
@@ -18,6 +18,23 @@ resource "aws_security_group" "allow_ssh1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
